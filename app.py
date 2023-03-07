486b0dc66226705ccba9233652eca62e4aedfe41
Sambo Chea <sombochea@cubetiqs.com>
2023-03-07 23:05:09 +0700

    Add export support

--- app.py
+++ app.py
@@ -17,14 +17,27 @@ def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
 
+def to_csv(usernames):
+    csv = 'no, username'
+    for i, username in enumerate(usernames):
+        csv += f'{i}, {username}'
+
+    return csv
+
+
 def create_app():
     app = Flask(__name__)
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
         tweet_id = request.args.get('tweet_id') or request.form.get('tweet_id')
+        export = request.args.get('export') or request.form.get('export')
         dl = request.args.get('dl') or request.form.get('dl')
         dl = str2bool(dl)
+        mine_type = 'text/csv' if export == 'csv' else 'application/json'
+        download_filename = f'export_{tweet_id}.csv' if export == 'csv' else f'export_{tweet_id}.json'
+        content_disposition_type = 'attachment' if dl else 'inline'
+        content_disposition = f"{content_disposition_type}; filename={download_filename}"
 
         if tweet_id:
             # Get the replies to the tweet
@@ -37,10 +50,27 @@ def create_app():
                 if reply.user.screen_name != author:
                     usernames.append(reply.user.screen_name)
 
+            uniqe_usernames = set(usernames)
+
+            if export == 'csv':
+                csv = to_csv(uniqe_usernames)
+                return Response(
+                    csv,
+                    mimetype=mine_type,
+                    headers={"Content-disposition": content_disposition}
+                )
+
+            if dl:
+                return Response(
+                    uniqe_usernames,
+                    mimetype=mine_type,
+                    headers={"Content-disposition": content_disposition}
+                )
+
             return make_response(jsonify({
                 'tweet_id': tweet_id,
                 'total_replies': len(usernames),
-                'unique_replies': len(set(usernames)),
+                'unique_replies': len(uniqe_usernames),
                 'author': author,
                 'replies': usernames,
             }), 200)
@@ -48,9 +78,13 @@ def create_app():
         return make_response(jsonify({
             'error': 'No tweet_id is provided',
             'params': {
-                'tweet_id': '1632730569065787392',
-                'lang': 'eng+khm',
+                'tweet_id': 'TWITTER_TWEET_ID',
+                'dl': '0/1',
+                'export': 'csv/json',
             },
+            # 'body': {
+            #     'TWEET_CONSUMER_KEY': 'stirng',
+            # },
             "examples": [
                 {
                     "uri": "/?tweet_id=1632730569065787392",
